498aa99b8876704f8951e335e696ca71fda00960
SyedGhullamMeeranGillani <meergillani999@icloud.com>
2022-07-28 19:00:13 +0500

    Initial Commit

---
+++ GoalsApp/GoalsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalsAppApp.swift
+//  GoalsApp
+//
+//  Created by Syed Ghullam Meeran Gillani on 28/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct GoalsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
